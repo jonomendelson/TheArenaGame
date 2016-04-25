28bcca1eaacace3084a14464bfd31f8e2c421f3f
Jonathan Mendelson <jonomendelson@gmail.com>
2016-04-25 15:49:53 -0400

    starting shadows

--- index.js
+++ index.js
@@ -181,6 +181,23 @@ function getItem(name){
 	}
 }
 
+function createEdge(vertex_a, vertex_b, player, parent_object){
+	var edge = {};
+	edge.vertex_a = vertex_a;
+	edge.vertex_b = vertex_b;
+	edge.midpoint = {xPos: (vertex_a[0]+vertex_b[0])/2, yPos: (vertex_a[0] + vertex_b[1])/2};
+	edge.distance = distFrom(edge.midpoint.xPos, edge.midpoint.yPos, player.xPos, player.yPos);
+	edge.parent_object = parent_object;
+	return edge;
+}
+
+function createVertex(point_a, point_b){
+	var vertex = {};
+	vertex.xPos = point_a;
+	vertex.yPos = point_b;
+	return vertex;
+}
+
 function solvePhysics(){
 	if(didFinishLoading){map_radius *= MAP_SIZE_DECAY;}
 		
@@ -719,37 +736,94 @@ function packageAllGameData(q){
 		var player_creatures = [];
 				
 		var vertices = [];
-				
-		for(var r = 0; r < obstacles.length; r++){
-			if(distFrom(obstacles[r].xPos, obstacles[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
-				player_obstacles.push(obstacles[r]);
-				vertices.push([obstacles[r].xPos, obstacles[r].yPos]);
-			}
-		}
+		var edges = [];
 				
 		for(var r = 0; r < players.length; r++){
-			if(distFrom(players[r].xPos, players[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
-				player_players.push(players[r]);
-				vertices.push([players[r].xPos, players[r].yPos]);
-			}
+				if(distFrom(players[r].xPos, players[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
+					player_players.push(players[r]);
+					var vertex1 = createVertex([players[r].xPos, players[r].yPos]);
+					vertices.push(vertex1);
+					var vertex2 = createVertex([players[r].xPos + BLOCK_SIZE, players[r].yPos]);
+					vertices.push(vertex2);
+					var vertex3 = createVertex([players[r].xPos+BLOCK_SIZE, players[r].yPos+BLOCK_SIZE]);
+					vertices.push(vertex3);
+					var vertex4 = createVertex([players[r].xPos, players[r].yPos+BLOCK_SIZE]);
+					vertices.push(vertex4);
+						
+
+					var edge1 = createEdge(vertex1, vertex2, players[q], players[r]);
+					edges.push(edge1);
+					var edge2 = createEdge(vertex2, vertex3, players[q], players[r]);
+					edges.push(edge2);
+					var edge3 = createEdge(vertex3, vertex4, players[q], players[r]);
+					edges.push(edge3);
+					var edge4 = createEdge(vertex4, vertex1, players[q], players[r]);
+					edges.push(edge4);
+				}
 		}
 				
 		for(var r = 0; r < items.length; r++){
-			if(distFrom(items[r].xPos, items[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
-				player_items.push(items[r]);
-				vertices.push([items[r].xPos, items[r].yPos]);
-			}
+				if(distFrom(items[r].xPos, items[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
+						player_items.push(items[r]);
+						var vertex1 = createVertex([items[r].xPos, items[r].yPos]);
+						vertices.push(vertex1);
+						var vertex2 = createVertex([items[r].xPos + BLOCK_SIZE, items[r].yPos]);
+						vertices.push(vertex2);
+						var vertex3 = createVertex([items[r].xPos+BLOCK_SIZE, items[r].yPos+BLOCK_SIZE]);
+						vertices.push(vertex3);
+						var vertex4 = createVertex([items[r].xPos, items[r].yPos+BLOCK_SIZE]);
+						vertices.push(vertex4);
+						
+
+						var edge1 = createEdge(vertex1, vertex2, players[q], items[r]);
+						edges.push(edge1);
+						var edge2 = createEdge(vertex2, vertex3, players[q], items[r]);
+						edges.push(edge2);
+						var edge3 = createEdge(vertex3, vertex4, players[q], items[r]);
+						edges.push(edge3);
+						var edge4 = createEdge(vertex4, vertex1, players[q], items[r]);
+						edges.push(edge4);
+				}
 		}
-		
+				
 		for(var r = 0; r < creatures.length; r++){
-			if(distFrom(creatures[r].xPos, creatures[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
-				player_creatures.push(creatures[r]);
-				vertices.push([creatures[r].xPos, creatures[r].yPos]);
-			}
+				if(distFrom(creatures[r].xPos, creatures[r].yPos, players[q].xPos, players[q].yPos) < MAX_VISION_RADIUS){
+						player_creatures.push(creatures[r]);
+						var vertex1 = createVertex([creatures[r].xPos, creatures[r].yPos]);
+						vertices.push(vertex1);
+						var vertex2 = createVertex([creatures[r].xPos + BLOCK_SIZE, creatures[r].yPos]);
+						vertices.push(vertex2);
+						var vertex3 = createVertex([creatures[r].xPos+BLOCK_SIZE, creatures[r].yPos+BLOCK_SIZE]);
+						vertices.push(vertex3);
+						var vertex4 = createVertex([creatures[r].xPos, creatures[r].yPos+BLOCK_SIZE]);
+						vertices.push(vertex4);
+						
+
+						var edge1 = createEdge(vertex1, vertex2, players[q], creatures[r]);
+						edges.push(edge1);
+						var edge2 = createEdge(vertex2, vertex3, players[q], creatures[r]);
+						edges.push(edge2);
+						var edge3 = createEdge(vertex3, vertex4, players[q], creatures[r]);
+						edges.push(edge3);
+						var edge4 = createEdge(vertex4, vertex1, players[q], creatures[r]);
+						edges.push(edge4);
+				}
 		}
 				
-		//add in stuff that removes everything in shadows
-				
+		edges.sort(function(a, b){
+ 				return a.distance-b.distance;
+		});
+		
+		for (var r = 0; r < vertices.length; r++){
+			var ray_slope = (vertices[r].yPos - players[q].yPos)/(vertices[r].xPos - players[q].qPos);
+			var ray_b = -ray_slope * vertices[r].xPos + vertices[r].yPos;
+			var above = (vertices[r].yPos >= players[q].yPos) ? true : false;
+			for (var n = 0; n < edges.length; n++){
+				var x = 
+			}
+		}
+
+		
 				
 		player_knowledge.stage = stage;
 		player_knowledge.players = player_players;
@@ -760,6 +834,7 @@ function packageAllGameData(q){
 		player_knowledge.map_center_y = MAP_CENTER_Y;
 		player_knowledge.items = player_items;
 		player_knowledge.creatures = player_creatures;
+		player_knowledge.draw_length = MAX_VISION_RADIUS;
 		
 		return player_knowledge;
 	
